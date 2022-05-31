d08fdf8b012f5adc8f4c5c143d64d51d82a1f685
Christian Areas <me@areas.me>
2022-05-30 22:50:07 -0500

    Finished part 4 of the Gatsby tutorial.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby GitHub`,
+    title: `Gatsby GitHub Sandbox`,
     siteUrl: `https://gatsbygithubmain.gtsb.io`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
